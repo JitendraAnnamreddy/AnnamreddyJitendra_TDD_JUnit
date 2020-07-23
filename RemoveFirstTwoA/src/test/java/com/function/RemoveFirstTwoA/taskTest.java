f6e46bb601b29a3d2d3b7dff1fb70d43acf1cada
JitendraAnnamreddy <jitendraannamreddy999@gmail.com>
2020-07-23 12:07:48 +0530

    TDD Task

---
+++ RemoveFirstTwoA/src/test/java/com/function/RemoveFirstTwoA/taskTest.java
@@ -0,0 +1,39 @@
+package com.function.RemoveFirstTwoA;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import com.function.RemoveFirstTwoA.*;
+
+public class taskTest {
+
+	removingA obj=new removingA();
+	@Test
+	public void test1() {
+		Assert.assertEquals("BCD", obj.removeAFunction("ABCD"));
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertEquals("CD", obj.removeAFunction("AACD"));
+	}
+	
+	@Test
+	public void test3() {
+		Assert.assertEquals("BCD", obj.removeAFunction("BACD"));
+	}
+	
+	@Test
+	public void test4() {
+		Assert.assertEquals("BBAA", obj.removeAFunction("BBAA"));
+	}
+	
+	@Test
+	public void test5() {
+		Assert.assertEquals("BAA", obj.removeAFunction("AABAA"));
+	}
+
+}
